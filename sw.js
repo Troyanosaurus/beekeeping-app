fa6a6fe84f2d2949fcc08caaa16bbeb4cb397245
Troyanosaurus <troybraguta2@gmail.com>
2025-10-04 01:24:53 +1300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+// sw.js
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', () => {}); // noop; enough for installability
